0b8ab40f84afb8a783627fafa4417a3f856c7066
Guilherme Boves <ghboves@gmail.com>
2023-02-06 13:25:07 -0300

    Sistema de busca

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
     <div class="column is-half is-offset-one-quarter">
-      <div v-for="(pokemon, index) in pokemons" :key="index">
+    <a href="/"><img id="img-logo" src="./assets/guia.png"></a>
+    <hr>
+    <h4 class="is-size-4">Pokedex</h4>
+    <input type="text" class="input is-rounded" placeholder="Buscar pokemon pelo nome" v-model="search">
+    <button id="searchBtn" @click="searchPokemon" class="button is-fullwidth is-success">Buscar</button>
+      <div v-for="(pokemon, index) in filteredPokemons" :key="pokemon.url">
         <Pokemon :name="pokemon.name" :url="pokemon.url" :num="index+1"/>
       </div>
     </div>
@@ -19,17 +24,43 @@ export default {
   name: 'App' ,
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      search: ''
     }
   },
   created: function(){
     axios.get(apiUrl+"pokemon?limit=151&offset=0").then(res => {
       this.pokemons = res.data.results;
-      console.log(this.pokemons)
+      this.filteredPokemons = res.data.results;
     })
   },
   components:{
     Pokemon
+  },
+  methods:{
+    searchPokemon: function(){
+      this.filteredPokemons = this.pokemons;
+      if(this.search == '' || this.search == ' '){
+        this.filteredPokemons = this.pokemons;
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.search)      
+        
+        if(this.filteredPokemons.length == 0){
+          //this.search == "";
+          this.filteredPokemons = this.pokemons;
+        }  
+      }
+    }
+  },
+  computed: {
+    // searchResult: function(){
+    //   if(this.search == '' || this.search == ' '){
+    //     return this.pokemons;
+    //   } else {
+    //     return this.pokemons.filter(pokemon => pokemon.name == this.search)
+    //   }
+    // }
   }
 }
 </script>
@@ -42,5 +73,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#img-logo{
+  background-color: blue;
+}
+#searchBtn{
+  margin-top: 2%;
 }
 </style>
